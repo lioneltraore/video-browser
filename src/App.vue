a8011ac7861f0d71f4af8e80b0b608b9968d23cf
Traore Leonel <ltraore@m2i-sa.com>
2024-02-29 22:48:47 +0000

    add serach component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
+  <SearchBar />
   Hi there
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from './components/SearchBar.vue';
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    SearchBar
   }
 }
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
 </style>
